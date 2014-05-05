ae8e5a98a7ea2dd0fab4a050d0ed5413dcb0af58
Eymen Gunay <eymen@egunay.com>
2014-05-05 03:20:37 +0200

    Composer extension added

--- README.md
+++ README.md
@@ -6,18 +6,28 @@ A wercker step to execute a `capistrano` task. The step has support for multiple
 More information:
 - http://capistranorb.com/
 
+# Included extensions
+
+* [composer](https://github.com/capistrano/composer)
+
 # Options
 
 * `tasks` (optional, default: `deploy`) The tasks which will be run.
-* `stage` (optional) The stage which will be used for the deployment. Requires [multistage extension](http://capistrano.org/cookbook/using-the-multistage-extension.html).
+* `stage` (optional, default: `production`) The stage which will be used for the deployment.
 
-# Example
+# Basic example
 
 ``` yaml
 deploy:
   steps:
-    - eymengunay/capistrano@0.0.1
+    - eymengunay/capistrano@0.0.2
 ```
+
+# Changing default task or stage
+
+You can change the default task to be executed by setting the `WERCKER_CAPISTRANO_TASKS` environment variable.
+Instead for stage setting you can use `WERCKER_CAPISTRANO_STAGE` environment variable. 
+
 
 # License
 
--- run.sh
+++ run.sh
@@ -1,23 +1,30 @@
 #!/bin/sh
 
-# First make sure capistrano is installed
-if ! type cap &> /dev/null ; then
-    if ! type gem &> /dev/null ; then
-        fail "neither capistrano nor ruby gem found."
-    else
-        info "capistrano was not found, installing"
-        sudo gem install capistrano
-    fi
-else
-    info "capistrano is available"
-    debug "capistrano version: $(capistrano --version)"
+if ! type gem &> /dev/null ; then
+    fail "ruby gem not found."
 fi
+
+# Capistrano
+$(gem query --name-matches 'capistrano' --installed)
+if [[ $? -ne 0 ]]; then
+    $(sudo gem install capistrano)
+fi
+
+# Capistrano composer
+$(gem query --name-matches 'capistrano-composer' --installed)
+if [[ $? -ne 0 ]]; then
+    $(sudo gem install capistrano-composer)
+fi
+
+debug "capistrano version: $(cap --version)"
 
 capistrano_command="cap"
 
 # Parse some variable arguments
 if [ -n "$WERCKER_CAPISTRANO_STAGE" ] ; then
     capistrano_command="$capistrano_command $WERCKER_CAPISTRANO_STAGE"
+else
+    capistrano_command="$capistrano_command production"
 fi
 
 if [ -n "$WERCKER_CAPISTRANO_TASKS" ] ; then
